64d970710947827708b42595422b5220b46dd7c2
Mihail <mihailgeorgiev94@gmail.com>
2020-05-31 08:59:20 +0300

    Add less support
    We have a liftoff; SpaceX got me to where I am

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -336,7 +336,7 @@ module.exports = function(webpackEnv) {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
@@ -369,7 +369,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -411,7 +411,7 @@ module.exports = function(webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
@@ -469,6 +469,15 @@ module.exports = function(webpackEnv) {
               // Remove this when webpack adds a warning or an error for this.
               // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true,
+            },{
+              test: /\.(?:le|c)ss$/,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 1,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                },
+                'less-loader'
+              )
             },
             // Adds support for CSS Modules, but using SASS
             // using the extension .module.scss or .module.sass
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchCurrentWeather, fetchFutureWeather, setWeatherUnits } from '../actions/weatherActions'
 import { WEATHER_UNITS } from '../consts';
 
+import './widget.less';
+
 import sunIcon from '../assets/sunIcon.png';
 import rainIcon from '../assets/rainIcon.png';
 
