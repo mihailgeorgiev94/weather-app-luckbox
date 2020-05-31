f92d4e031e40f7045741ead2071a28d2bb7ae638
Mihail <mihailgeorgiev94@gmail.com>
2020-05-31 22:45:23 +0300

    Add parallax

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import { store } from './store';
 
 import { Widget, WeatherDetails, FutureWeather } from './components';
 
+import sunBackground from './assets/sunBackground.png';
+import rainBackground from './assets/rainBackground.png';
+
 import './app.less';
 
-export const App = () => {
-  return (
-    <Provider store={store}>
-      <div className="app">
+export const App = () => (
+  <Provider store={store}>
+    <div className="app">
+      <div className="parallax" style={{backgroundImage: `url(${rainBackground})`}}/>
+      <div className="widget-background"/>
+      <div className="widget-wrapper">
         <Widget/>
+      </div>
+      <div className="content">
         <WeatherDetails/>
         <FutureWeather/>
       </div>
-    </Provider>
-  );
-};
+    </div>
+  </Provider>
+);
