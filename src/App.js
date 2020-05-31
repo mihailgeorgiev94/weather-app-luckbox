70a16985dc76ebc0b14fc8c7f6c83b55bbc3c6a7
Mihail <mihailgeorgiev94@gmail.com>
2020-05-31 23:10:05 +0300

    Add change color on scroll position

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useCallback, useRef } from 'react';
 import { Provider } from 'react-redux';
 
 import { store } from './store';
@@ -10,11 +10,34 @@ import rainBackground from './assets/rainBackground.png';
 
 import './app.less';
 
-export const App = () => (
+const BACKGROUND_SUN = "#EEB625";
+const MAIN_PEACH = "#EEA594";
+
+export const App = () => {
+  const appRef = useRef(null);
+  const widgetBackgroundRef = useRef(null);
+
+  const listenScrollEvent = useCallback(() => {
+    if(window.scrollY > window.innerHeight / 1.3) {
+      appRef.current.style.background = MAIN_PEACH;
+      widgetBackgroundRef.current.style.background = MAIN_PEACH;
+    } else {
+      appRef.current.style.background = BACKGROUND_SUN;
+      widgetBackgroundRef.current.style.background = BACKGROUND_SUN;
+    }
+  }, [appRef]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent)
+    appRef.current.style.background = BACKGROUND_SUN
+    widgetBackgroundRef.current.style.background = BACKGROUND_SUN
+  });
+
+  return (
   <Provider store={store}>
-    <div className="app">
+    <div ref={appRef} className="app">
       <div className="parallax" style={{backgroundImage: `url(${rainBackground})`}}/>
-      <div className="widget-background"/>
+      <div ref={widgetBackgroundRef} className="widget-background"/>
       <div className="widget-wrapper">
         <Widget/>
       </div>
@@ -24,4 +47,4 @@ export const App = () => (
       </div>
     </div>
   </Provider>
-);
+)};
