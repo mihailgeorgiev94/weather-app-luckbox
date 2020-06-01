371856ee15d3da8407f97a5af174d4ff6f266602
Mihail <mihailgeorgiev94@gmail.com>
2020-06-01 06:00:29 +0300

    Change backgrounds based on weatherType

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,6 +2,7 @@ import React, { useEffect, useCallback, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Widget, WeatherDetails, FutureWeather } from './';
+import { WEATHER_TYPES } from '../consts';
 
 import sunBackground from '../assets/sunBackground.png';
 import rainBackground from '../assets/rainBackground.png';
@@ -9,22 +10,27 @@ import rainBackground from '../assets/rainBackground.png';
 import './weather.less';
 
 const BACKGROUND_SUN = "#EEB625";
+const BACKGROUND_RAIN = "#61A9A6";
 const MAIN_PEACH = "#EEA594";
 
 export const Weather = () => {
   const weatherRef = useRef(null);
   const widgetBackgroundRef = useRef(null);
-  const { city } = useSelector(state => state.weather);
+  const { city, weatherType } = useSelector(state => state.weather);
+
+  const isSunny = Object.values(WEATHER_TYPES).includes(weatherType)
 
   const listenScrollEvent = useCallback(() => {
+    let color = '';
     if(window.scrollY > (window.innerHeight / 1.3 - 30)) {
-      weatherRef.current.style.background = MAIN_PEACH;
-      widgetBackgroundRef.current.style.background = MAIN_PEACH;
+      color = MAIN_PEACH;
     } else {
-      weatherRef.current.style.background = BACKGROUND_SUN;
-      widgetBackgroundRef.current.style.background = BACKGROUND_SUN;
+      color = isSunny ? BACKGROUND_SUN : BACKGROUND_RAIN
     }
-  }, [weatherRef]);
+
+    weatherRef.current.style.background = color;
+    widgetBackgroundRef.current.style.background = color;
+  }, [weatherRef, isSunny]);
 
   useEffect(() => {
     window.addEventListener('scroll', listenScrollEvent)
@@ -34,7 +40,10 @@ export const Weather = () => {
 
   return (
     <div ref={weatherRef} className="weather">
-      <div className="parallax" style={{backgroundImage: `url(${rainBackground})`}}/>
+      <div
+        className="parallax"
+        style={{ backgroundImage: `url(${isSunny ? sunBackground : rainBackground})` }}
+      />
       <div ref={widgetBackgroundRef} className="widget-background"/>
       <div className="widget-wrapper">
         <Widget/>
